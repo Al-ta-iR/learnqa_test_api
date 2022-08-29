7ccb16fb7246f7e86241eb8adf41f1d367e39f84
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-29 15:29:43 +0200

    try-except block with check answer in json-format

--- case_1.py
+++ case_1.py
@@ -1,8 +1,15 @@
+from json import JSONDecodeError
 import requests
 
 
 # payload = {"name": "Mike"}
-response = requests.get('http://playground.learnqa.ru/api/hello', params={"name": "Mike"})
-parsed_response_text = response.json()
+response = requests.get('http://playground.learnqa.ru/api/get_text')
+print(response.text)
 
-print(parsed_response_text["answer"])
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text["answer"])
+except JSONDecodeError:
+    print("Наш ответ получен не в JSON формате")
+finally:
+    pass
