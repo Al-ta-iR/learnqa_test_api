b12642e07f524367cf5b4a08633c257701bf16f5
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-29 16:41:49 +0200

    parameterized test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,11 +1,19 @@
 
+import pytest
 import requests
 
 
 class TestFirstAPI:
-    def test_hello_call(self):
+    names = [
+        ("Mike"),
+        ("Olga"),
+        ()
+    ] # добавили словарь с кортежами, содержащие имена
+
+    @pytest.mark.parametrize('name', names) # довесили pytest-вое переопределение (как функциональная дообёртка) функции-тесту, которая добавляет новые возможности (прогонка значений указанных переменных). Переменной 'name' в ней будут подставляться значения из нашего словаря names
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Mike"
+        # удалили прошлое конкретизированное установление имени. Остальное остаётся прежним
         data = {"name": name}
 
         response = requests.get(url, params=data)
@@ -15,6 +23,10 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no filed 'answer' in response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) > 0:
+            expected_response_text = f"Hello, {name}"
+        else:
+            expected_response_text = "Hello, someone"
+
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text , "Actual text in the response is not correct"
