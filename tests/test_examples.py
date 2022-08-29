8b9f88a4ac90626fdbc94216969ea43d8d678ec1
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-29 16:34:19 +0200

    first api-test with pytest

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,11 +1,20 @@
-class TestExample:
-    def test_check_math(self):
-        a = 5
-        b = 9
-        assert a + b == 14, "We wait 14 here"
+
+import requests
 
 
-    def test_check_math2(self):
-        a = 5
-        b = 10
-        assert a + b == 11, "We wait 11 here"
+class TestFirstAPI:
+    def test_hello_call(self):
+        url = "https://playground.learnqa.ru/api/hello"
+        name = "Mike"
+        data = {"name": name}
+
+        response = requests.get(url, params=data)
+
+        assert response.status_code == 200, "Wrong response code"
+
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no filed 'answer' in response"
+
+        expected_response_text = f"Hello, {name}"
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_response_text , "Actual text in the response is not correct"
