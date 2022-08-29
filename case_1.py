a1b736388768a307a819f9f2c193853fc552cf5b
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-29 15:11:04 +0200

    add name to params in get api/hello

--- case_1.py
+++ case_1.py
@@ -2,6 +2,6 @@ import requests
 
 
 payload = {"name": "Mike"}
-response = requests.get('http://playground.learnqa.ru/api/check_type', params=payload)
+response = requests.get('http://playground.learnqa.ru/api/hello', params=payload)
 
 print(response.text)
