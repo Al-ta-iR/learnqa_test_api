7321b3413df849fb83046b1c7e45902b95fc9a2a
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-29 15:45:35 +0200

    login with true and false passwords

--- case_1.py
+++ case_1.py
@@ -1,9 +1,18 @@
-from json import JSONDecodeError
 import requests
 
 
-headers = {"some_headers": "123"}
-response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+payload = {"login": "secret_login", "password": "secret_pass"}
+response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
 
 print(response.text)
+print(response.status_code)
+print(dict(response.cookies)) # добавили функцию dict для удобочитаемости
 print(response.headers)
+print("_________________________________")
+
+payload = {"login": "secret_login", "password": "secret_pass111"}
+response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+
+print(response.text)
+print(response.status_code)
+print(dict(response.cookies)) # добавили функцию dict для удобочитаемости
