e85c198d006df6a11d4106dd9ee9ba99b88ea15c
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-29 15:23:07 +0200

    internal params in requests

--- case_1.py
+++ case_1.py
@@ -1,8 +1,8 @@
 import requests
 
 
-payload = {"name": "Mike"}
-response = requests.get('http://playground.learnqa.ru/api/hello', params=payload)
+# payload = {"name": "Mike"}
+response = requests.get('http://playground.learnqa.ru/api/hello', params={"name": "Mike"})
 parsed_response_text = response.json()
 
 print(parsed_response_text["answer"])
