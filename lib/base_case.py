12f0878105985bcc149fd27c426218b7f427d12d
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-31 15:30:25 +0200

    edit user data

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,5 @@
 import json.decoder
-
+from datetime import datetime
 from requests import Response
 
 
@@ -13,6 +13,7 @@ class BaseCase:
         assert headers_name in response.headers, f"Cannot find header with the name {headers_name} in the last response"
         return response.headers[headers_name]
 
+
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
@@ -21,3 +22,18 @@ class BaseCase:
         
         assert name in response_as_dict, "Response JSON does not have key '{name}'"
         return response_as_dict[name]
+
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = 'learnqa'
+            domain = 'example.com'
+            dynamic_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{dynamic_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
