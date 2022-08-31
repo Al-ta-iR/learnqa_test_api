8be0461cd042b61753b495fd5f16620427ecce21
Al-ta-iR <artyom.oleynik@gmail.com>
2022-08-31 17:16:52 +0200

    add requests class

---
+++ lib/my_requests.py
@@ -0,0 +1,48 @@
+from urllib import request, response
+
+
+import requests
+
+
+class MyRequests():
+    @staticmethod
+    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, 'POST')
+
+
+    @staticmethod
+    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, 'GET')
+
+
+    @staticmethod
+    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, 'PUT')
+
+
+    @staticmethod
+    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, 'DELETE')
+
+
+    @staticmethod
+    def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
+
+        url = f"https://playground.learnqa.ru/api{url}"
+        if headers is None:
+            headers = {}
+        if cookies is None:
+            cookies = {}
+
+        if method == 'GET':
+            response = request.get(url, params=data, headers=headers, cookies=cookies)
+        elif method == 'POST':
+            response = request.post(url, data=data, headers=headers, cookies=cookies)
+        elif method == 'PUT':
+            response = request.put(url, data=data, headers=headers, cookies=cookies)
+        elif method == 'DELETE':
+            response = request.delete(url, data=data, headers=headers, cookies=cookies)
+        else:
+            raise Exception(f"Bad HTTP method '{method}' was received")
+
+        return response
